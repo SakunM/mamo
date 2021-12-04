2ec99e72af04fbcf3198be0af6dadbf915dbffaa
SakunM <mamoru.sakuma@gmail.com>
2021-12-05 07:48:32 +0900

    add  Rust

---
+++ langs/rust/prjs/isses/target/debug/deps/isses.d
@@ -0,0 +1,5 @@
+A:\PJ\mamo\langs\rust\prjs\isses\target\debug\deps\isses.exe: src\main.rs
+
+A:\PJ\mamo\langs\rust\prjs\isses\target\debug\deps\isses.d: src\main.rs
+
+src\main.rs:
---
+++ langs/rust/prjs/isses/target/debug/isses.d
@@ -0,0 +1,1 @@
+A:\PJ\mamo\langs\rust\prjs\isses\target\debug\isses.exe: A:\PJ\mamo\langs\rust\prjs\isses\src\main.rs
